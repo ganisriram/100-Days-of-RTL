f58a4893b14b3e44359829b231fc11726cdc1e87
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-16 19:17:12 +0530

    Update design.v

--- Verilog Modelling Styles [Day 1]/Behaviour Model/design.v
+++ Verilog Modelling Styles [Day 1]/Behaviour Model/design.v
@@ -1,1 +1,16 @@
-
+module logic_gates(
+  input a,b,
+  output reg and_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate);
+  always@(*)
+    begin
+      and_gate=a&b;
+      or_gate=a|b;
+      not_gate=~a;
+      nand_gate=~(a&b);
+      nor_gate=~(a|b);
+      xor_gate=a^b;
+      xnor_gate=~(a^b);
+    end
+endmodule
+  
+  
