4bda874856965b81848d5096c1c6788db438c34d
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-17 16:43:51 +0530

    Update design.v

--- Verilog Modelling Styles [Day 1]/structural model/design.v
+++ Verilog Modelling Styles [Day 1]/structural model/design.v
@@ -1,1 +1,12 @@
-
+module logic_gates(
+  input wire a,b;
+  output reg and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate;
+);
+  and w1(and_gate,a,b);
+  or w2(or_gate,a,b);
+  not w3(not_gate,a,b);
+  nand w4(nand_gate,a,b);
+  nor w5(nor_gate,a,b);
+  xor w6(xor_gate,a,b);
+  xnor w7(xnor_gate,a,b);
+endmodule
