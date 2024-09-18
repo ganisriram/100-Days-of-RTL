4c99f1af8f3bd2f947df2f9e84d45c66f85f136b
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-18 18:14:10 +0530

    Update Design.v

--- Half Adder[Day2]/Using NOR/Design.v
+++ Half Adder[Day2]/Using NOR/Design.v
@@ -1,1 +1,9 @@
 
+module ha_nor(input a,b,output reg carry);
+  wire w1,w2,w3;
+  assign w1=~(a|a);
+  assign w2=~(b|b);
+  assign  w3=~(a|b);
+  assign carry=~(w1|w2);
+  assign summ=~(carry|w3);
+endmodule
