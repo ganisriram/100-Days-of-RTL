eb1ba685a57b7f9d5b1523eb83a8f6a081a5f120
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-19 14:34:32 +0530

    Update Design.v

--- Half Adder[Day2]/Using NAND/Design.v
+++ Half Adder[Day2]/Using NAND/Design.v
@@ -3,8 +3,8 @@
 module ha_nand(input a,b,output reg carry);
   wire w1,w2,w3;
   assign w1=~(a&b);
-  assign w2=~(w1|a);
-  assign  w3=~(w1|b);
-  assign carry=~(w1|w1);
-  assign summ=~(w2|w3);
+  assign w2=~(w1&a);
+  assign  w3=~(w1&b);
+  assign carry=~(w1&w1);
+  assign summ=~(w2&w3);
 endmodule
