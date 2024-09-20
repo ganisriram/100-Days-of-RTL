f086fba3e50840ef3ae3b658acfbf5d101e5a23a
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-20 17:25:06 +0530

    Update testbench.v

--- Half Subtractor[Day 4]/Using Nand/testbench.v
+++ Half Subtractor[Day 4]/Using Nand/testbench.v
@@ -1,1 +1,11 @@
-
+`include "design.v"
+module half_nand_tb(output reg a,b,input wire differ,borrow);
+  half_nand dut(a,b,differ,borrow);
+  initial begin
+    $monitor("Time=%0t a=%b b=%b differ=%b borrow=%b",$time,a,b,differ,borrow);
+    #5 a=0;b=0;
+    #5 a=0;b=1;
+    #5 a=1;b=0;
+    #5 a=1;b=1;
+  end
+    endmodule
