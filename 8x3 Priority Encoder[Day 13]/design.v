210b6bf271b8e0d9d4cc255af4b4fd082310f9ad
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-29 18:53:50 +0530

    Update design.v

--- 8x3 Priority Encoder[Day 13]/design.v
+++ 8x3 Priority Encoder[Day 13]/design.v
@@ -1,1 +1,7 @@
-
+module priorityencoderex3 (d,b0,b1,b2);
+input [7:0]d;
+output bo,b1,b2;
+  assign b2=d[7]|d[6]| [d[5]|d[4];
+  assign bl=d[7] |[d[6] | (-d[5]& ~d[4] &d[3]) | (-d[5] &~d[4] &d[2]);
+  assign bo=d[7]| ((~d[6]) &d[5])| ((-d[6]) & (~d[4]) &d[3]) | ((~d[6])& (~d[4])& (-d[2])& d[1]):
+endmodule
