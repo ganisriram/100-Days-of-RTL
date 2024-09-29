1cfe4405212c8f16b7b6a5e0acb11a744bca9937
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-09-29 19:00:29 +0530

    Update testbench.v

--- 8x3 Priority Encoder[Day 13]/testbench.v
+++ 8x3 Priority Encoder[Day 13]/testbench.v
@@ -1,1 +1,19 @@
+module priorityencoder8x3_th;
+reg [7:01d;
+wire bo,b1,b2;
+priorityencoder8x3 dut (.d(d),.bo (bo), .bi (bl), .b2 (b2));
+Initial begin
+ #10 ;d=8'b00000001;
+ #10 ;d=8'b0000001x;
+ #10 ;d=8'b000001xx;
+ #10 ;d=8'b00001xxx;
+ #10 ;d=8'b0001xxxx;
+ #10 ;d=8'b001xxxxx;
+ #10 ;d=8'b01xxxxxx;
+ #10 ;d=8'b1xxxxxxx;
+#10;
+  $finish;
+end
+
+endmodule
 
