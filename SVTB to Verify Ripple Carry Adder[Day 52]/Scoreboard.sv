a375dfbca4b2f0869e64b05cb96d1c1279bb6fcc
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-16 10:17:06 +0530

    Update Scoreboard.sv

--- SVTB to Verify Ripple Carry Adder[Day 52]/Scoreboard.sv
+++ SVTB to Verify Ripple Carry Adder[Day 52]/Scoreboard.sv
@@ -1,1 +1,27 @@
+class scoreboard;
+  transaction rm_data, rcvdata;
+  mailbox #(transaction) ref2scr;
+  mailbox #(transaction) rdmon2sc;
 
+  function new(mailbox #(transaction) ref2scr, mailbox #(transaction) rdmon2sc);
+    this.ref2scr = ref2scr;
+    this.rdmon2sc = rdmon2sc;
+  endfunction
+
+  virtual task start();
+      forever begin
+        rm_data=new();
+        ref2scr.get(rm_data);
+        rcvdata=new();
+        rdmon2sc.get(rcvdata);
+        check(rcvdata);
+      end
+  endtask
+  virtual task check(transaction rc_data);
+    if ((rm_data.sum== rc_data.sum)&&(rm_data.carryout== rc_data.carryout))
+      $display("compared sucessfully");
+        else
+          $display("not compared");
+  endtask
+
+endclass
