68861efc89c7a53d6298e020cc68d5668b1f9707
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-16 10:15:22 +0530

    Update Transaction.sv

--- SVTB to Verify Ripple Carry Adder[Day 52]/Transaction.sv
+++ SVTB to Verify Ripple Carry Adder[Day 52]/Transaction.sv
@@ -1,1 +1,10 @@
-
+class transaction;
+   rand bit [3:0]a;
+   rand bit [3:0]b;
+  rand bit cin;
+   bit [3:0]sum;
+   bit carryout;
+  function void display(input string tag);
+     $display("Time:%0t [%0s]: a:%0b b:%0b cin:%b sum:%b carry:%0b",$time, tag, a,b,cin,sum,carryout);
+  endfunction
+endclass
