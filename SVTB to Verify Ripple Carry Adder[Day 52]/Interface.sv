3e524a10cb41f7f9e5fcbac4d18d468c3131e9d9
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-16 10:15:04 +0530

    Update Interface.sv

--- SVTB to Verify Ripple Carry Adder[Day 52]/Interface.sv
+++ SVTB to Verify Ripple Carry Adder[Day 52]/Interface.sv
@@ -1,1 +1,11 @@
-
+interface intf(); 
+  logic [3:0]a;
+  logic[3:0] b;
+  logic cin;
+  logic [3:0]sum;
+  logic carryout;
+  
+  modport WR_DRV_MP (output a,b,cin);
+  modport WR_MON_MP (input a,b,cin);
+  modport RD_MON_MP ( input sum,carryout);
+endinterface
