665b88b71c0b4ca5d16f97c820d36dbc742a2568
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-17 21:22:54 +0530

    Update interface.sv

--- SVTB to Verify Decoder[Day 53]/interface.sv
+++ SVTB to Verify Decoder[Day 53]/interface.sv
@@ -1,1 +1,12 @@
-
+interface intf(); 
+  logic a;
+  logic b;
+ logic y0;
+  logic y1;
+  logic y2;
+  logic y3;
+  
+  modport WR_DRV_MP (output a,b);
+  modport WR_MON_MP (input a,b);
+  modport RD_MON_MP ( input y0,y1,y2,y3);
+endinterface
