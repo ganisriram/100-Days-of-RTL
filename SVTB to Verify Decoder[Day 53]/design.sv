407a41b160c21130defdeb12199e0d5ab497def3
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-17 21:22:14 +0530

    Update design.sv

--- SVTB to Verify Decoder[Day 53]/design.sv
+++ SVTB to Verify Decoder[Day 53]/design.sv
@@ -1,1 +1,9 @@
 
+module decoder2x4(a,b,y0,y1,y2,y3);
+  input wire a,b;
+  output reg y0,y1,y2,y3;
+  assign y0=(~a) & (~b);
+   assign y1=(~a) & (b);
+  assign y2=(a) & (~b);
+   assign y3=(a) & (b);
+endmodule
