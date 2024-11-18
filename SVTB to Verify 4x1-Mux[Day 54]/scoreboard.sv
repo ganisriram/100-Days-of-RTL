51b71b9374e6752f3ff41983bad40aa25d80cfea
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:12:30 +0530

    Update scoreboard.sv

--- SVTB to Verify 4x1-Mux[Day 54]/scoreboard.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/scoreboard.sv
@@ -1,1 +1,28 @@
 
+class scoreboard;
+  transaction rm_data, rcvdata;
+  mailbox #(transaction) ref2scr;
+  mailbox #(transaction) rdmon2sc;
+
+  function new(mailbox #(transaction) ref2scr, mailbox #(transaction) rdmon2sc);
+    this.ref2scr = ref2scr;
+    this.rdmon2sc = rdmon2sc;
+  endfunction
+
+  virtual task start();
+      forever begin
+        rm_data=new();
+        ref2scr.get(rm_data);
+        rcvdata=new();
+        rdmon2sc.get(rcvdata);
+        check(rcvdata);
+      end
+  endtask
+  virtual task check(transaction rc_data);
+    if (rm_data.y== rc_data.y)
+      $display("compared sucessfully");
+        else
+          $display("not compared");
+  endtask
+
+endclass
