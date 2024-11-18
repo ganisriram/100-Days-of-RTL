8c09551fc39f96f9f1b6ff744a2fe6c24a90a58f
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:11:20 +0530

    Update read_monitor.sv

--- SVTB to Verify 4x1-Mux[Day 54]/read_monitor.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/read_monitor.sv
@@ -1,1 +1,19 @@
-
+class rdmonitor;
+  virtual intf.RD_MON_MP rd_mon_inf;
+  transaction rddata;//data2scr;
+  mailbox #(transaction) rdmon2sc;
+  
+  function new(virtual intf.RD_MON_MP rd_mon_inf, mailbox #(transaction) rdmon2sc);
+    this.rd_mon_inf = rd_mon_inf;
+    this.rdmon2sc = rdmon2sc;
+    this.rddata = new();
+  endfunction
+  virtual task monitor();
+    forever begin
+ #5;
+      rddata.y = rd_mon_inf.y;  
+    rddata.display("DATA FROM READ MONITOR");
+     rdmon2sc.put(rddata);
+    end
+  endtask
+endclass
