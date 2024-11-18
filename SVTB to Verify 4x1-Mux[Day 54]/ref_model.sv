c10088348b974b705bbd0e72684dda2f35d3f802
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:12:04 +0530

    Update ref_model.sv

--- SVTB to Verify 4x1-Mux[Day 54]/ref_model.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/ref_model.sv
@@ -1,1 +1,25 @@
+class refmodel;
+  transaction wrmon_data;
+  mailbox #(transaction) wr2ref;
+  mailbox #(transaction) ref2scr;
 
+  function new (mailbox #(transaction)wr2ref, mailbox # (transaction) ref2scr);
+    this.wr2ref=wr2ref;
+    this.ref2scr=ref2scr;
+  endfunction
+  virtual task ref_logic(transaction wrmon_data);
+    wrmon_data.y=~(wrmon_data.s1)&~(wrmon_data.s2) &(wrmon_data.a)|~(wrmon_data.s1)&(wrmon_data.s2) &(wrmon_data.b)| (wrmon_data.s1)&~(wrmon_data.s2) &(wrmon_data.c) | (wrmon_data.s1)&(wrmon_data.s2) &(wrmon_data.d);
+   
+  endtask
+    
+  virtual task start();
+      forever 
+        begin
+        wrmon_data=new();
+        wr2ref.get(wrmon_data);
+          ref_logic(wrmon_data);
+          ref2scr.put(wrmon_data);
+          wrmon_data.display("refmodell");
+        end
+  endtask
+endclass
