f122701486cb3e8d5d002d945fea74e66cacfd6c
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:08:59 +0530

    Update interface.sv

--- SVTB to Verify 4x1-Mux[Day 54]/interface.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/interface.sv
@@ -1,1 +1,13 @@
-
+interface intf(); 
+  logic a;
+  logic b;
+  logic c;
+  logic d;
+  logic s1;
+  logic s2;
+  logic y;
+  
+  modport WR_DRV_MP (output a,b,c,d,s1,s2);
+  modport WR_MON_MP (input a,b,c,d,s1,s2);
+  modport RD_MON_MP ( input y);
+endinterface
