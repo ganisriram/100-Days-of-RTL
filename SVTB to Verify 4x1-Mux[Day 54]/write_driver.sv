d7e657b20723f3372bdb6fb556a53cebf4840ad4
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:10:35 +0530

    Update write_driver.sv

--- SVTB to Verify 4x1-Mux[Day 54]/write_driver.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/write_driver.sv
@@ -1,1 +1,21 @@
-
+class driver;
+  virtual intf.WR_DRV_MP wr_drv_inf;
+  transaction data2drv;
+  mailbox #(transaction)gen2wr;
+  function new(virtual intf.WR_DRV_MP  wr_drv_inf, mailbox #(transaction)gen2wr);
+    this.wr_drv_inf = wr_drv_inf;
+    this.gen2wr = gen2wr;
+  endfunction
+  virtual task drive();
+    forever begin
+    gen2wr.get(data2drv);
+     wr_drv_inf.a <= data2drv.a;
+       wr_drv_inf.b <= data2drv.b;
+       wr_drv_inf.c <= data2drv.c;
+      wr_drv_inf.d <= data2drv.d;
+      wr_drv_inf.s1 <=data2drv.s1;
+      wr_drv_inf.s2<=data2drv.s2;
+        data2drv.display("DRV");
+    end
+  endtask
+endclass
