1688f878be0032749118f468ddaaad53be8f7df7
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-18 22:09:32 +0530

    Update transaction.sv

--- SVTB to Verify 4x1-Mux[Day 54]/transaction.sv
+++ SVTB to Verify 4x1-Mux[Day 54]/transaction.sv
@@ -1,1 +1,12 @@
-
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit c;
+  rand bit d;
+   rand bit s1;
+   rand bit s2;
+   bit y;
+  function void display(input string tag);
+    $display("Time:%0t [%0s]: a:%0b b:%0b c:%0b d:%0b s1:%0b s2:%0b y:%0b",$time, tag, a,b,c,d,s1,s2,y);
+  endfunction
+endclass
