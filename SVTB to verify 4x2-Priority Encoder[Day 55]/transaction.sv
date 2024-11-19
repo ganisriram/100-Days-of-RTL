2bef964ca835c5791f6d45b14d81d1d13263e1d1
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-19 19:26:36 +0530

    Update transaction.sv

--- SVTB to verify 4x2-Priority Encoder[Day 55]/transaction.sv
+++ SVTB to verify 4x2-Priority Encoder[Day 55]/transaction.sv
@@ -1,1 +1,11 @@
-
+ class transaction;
+   rand bit a;
+   rand bit b;
+   rand bit c;
+   rand bit d;
+   bit y1;
+   bit y2;
+  function void display(input string tag);
+    $display("Time:%0t [%0s]: a:%0b b:%0b c:%b d:%b y1:%0b y2:%0b",$time, tag, a,b,c,d,y1,y2);
+  endfunction
+endclass
