fcbba4845ca8c50159289004f75d4ca4f9bc971e
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-19 19:29:26 +0530

    Update top.sv

--- SVTB to verify 4x2-Priority Encoder[Day 55]/top.sv
+++ SVTB to verify 4x2-Priority Encoder[Day 55]/top.sv
@@ -1,1 +1,24 @@
-
+`include "design.sv"
+`include "interface.sv"
+`include "Transaction.sv"
+`include "generator.sv"
+`include "wr_driver.sv"
+`include "wr_mon.sv"
+`include "rd_mon.sv"
+`include "ref_model.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "test.sv"
+module top;
+  intf duv_if();
+  test test_h;
+  encoder4x2 dut(.a(duv_if.a), .b(duv_if.b), .c(duv_if.c), .d(duv_if.d), .y1(duv_if.y1), .y2(duv_if.y2));
+  initial begin
+    test_h=new(duv_if,duv_if,duv_if);
+    test_h.build_run();
+    #100;     
+    $finish;
+  end
+endmodule
+  
+               
