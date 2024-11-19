b9483274c431d04e16129d637b21700c449a1209
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-19 19:32:26 +0530

    Update interface.sv

--- SVTB to verify 4x2-Priority Encoder[Day 55]/interface.sv
+++ SVTB to verify 4x2-Priority Encoder[Day 55]/interface.sv
@@ -1,1 +1,11 @@
-
+interface intf;
+  logic a;
+  logic b;
+  logic c;
+  logic d;
+  logic y1;
+  logic y2;
+  modport WR_DRV_MP(output a,b,c,d);
+  modport WR_MON_MP(input a,b,c,d);
+  modport RD_MON_MP(input y1,y2);
+endinterface
