93d0db8f72efa28b44fc4d58747e7aebb0778df0
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-20 20:39:36 +0530

    Update transaction.sv

--- SVTB to verify Logic Gates using Mux[Day 56]/transaction.sv
+++ SVTB to verify Logic Gates using Mux[Day 56]/transaction.sv
@@ -1,1 +1,15 @@
 
+ class transaction;
+   rand bit a;
+   rand bit b;
+        bit y1;
+       bit y2;
+       bit y3;
+       bit  y4;
+       bit y5;
+       bit y6;
+  function void display(input string tag);
+    $display("Time:%0t [%0s]: a:%0b b:%0b y1:%0b y2:%0b y3:%0b y4:%0b y5:%0b y6:%0b ",$time,tag,a,b,y1,y2,y3,y4,y5,y6);
+  endfunction
+endclass
+
