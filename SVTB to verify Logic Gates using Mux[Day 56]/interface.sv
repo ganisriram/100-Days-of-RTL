42854ab058d011a10dc869ef95b2c1addb5fecb5
ganisriram <139797762+ganisriram@users.noreply.github.com>
2024-11-20 20:39:10 +0530

    Update interface.sv

--- SVTB to verify Logic Gates using Mux[Day 56]/interface.sv
+++ SVTB to verify Logic Gates using Mux[Day 56]/interface.sv
@@ -1,1 +1,14 @@
-
+interface intf(); 
+  logic a;
+  logic b;
+ logic y1;
+  logic y2;
+  logic y3;
+  logic y4;
+  logic  y5;
+  logic y6;
+  
+  modport WR_DRV_MP (output a,b);
+  modport WR_MON_MP (input a,b);
+  modport RD_MON_MP ( input y1,y2,y3,y4,y5,y6);
+endinterface
